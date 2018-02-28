3fe72510298f6141eb91648f9b308fa1606b56c2
Florent Poinsaut <florent@poinsaut.fr>
2018-02-28 16:22:50 +0100

    Add HTTPs redirection option

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,13 @@
+var adminConfig = require('../config/adminconfig.js');
+
 module.exports = function(app, connection) {
+
+app.all('*', function(req, res, next) {
+  if(req.secure || !adminConfig.https_redirect){
+    return next();
+  };
+  res.redirect('https://' + req.host + req.url);
+});
 
 // Handle / root route.
 app.get('/', function(req, res) {
